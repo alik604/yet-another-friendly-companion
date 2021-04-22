f4a01f49f3e498f313167a6d29e9585be09c3fac
K. Ali Pardhan <kpardhan@sfu.ca>
2021-04-22 16:29:45 -0700

    Update hinn_eval.py

--- far_ws/src/follow_ahead_rl/scripts/hinn_eval.py
+++ far_ws/src/follow_ahead_rl/scripts/hinn_eval.py
@@ -1,4 +1,4 @@
-from time import sleep
+from time import sleep, time
 import random
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -20,8 +20,7 @@ RANDOMSEED = 42
 N_EPISODES = 100
 EPISODE_LEN = 15 # Seconds
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
+EVALUATE_PIPELINE = True
 
 if __name__ == '__main__':
     env = gym.make(ENV_NAME).unwrapped
@@ -33,15 +32,24 @@ if __name__ == '__main__':
     state_dim = env.observation_space.shape[0]
     action_dim = env.action_space.shape[0]
 
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
     model = HumanIntentNetwork(inner=128, input_dim=state_dim, output_dim=3)
     model.load_checkpoint()
     model.to(device)
 
     dis_heuristic = DistanceHeuristic()
 
+    cumulative_reward_per_episode = []
+    cumulative_reward = 0
     mode = 0
     for i in range(N_EPISODES):
+        if EVALUATE_PIPELINE:
+            if (mode % 5) == 3:     # Don't use random person path
+                mode += 1
         env.set_person_mode(mode % 5)
+        print(f"Running Episode {i} Person Mode = {mode % 5}")
+        start = time()
         mode += 1
         state = env.reset()
 
@@ -50,10 +58,23 @@ if __name__ == '__main__':
             state_tensor = torch.Tensor(state).to(device)
             ps_prediction = model.forward(state_tensor)
 
-            action = dis_heuristic.calculate_vector(ps_prediction, [])
+            action = dis_heuristic.calculate_vector(ps_prediction)
             
             env.set_marker_pose([ps_prediction[0],ps_prediction[1]])
             sleep(0.1)
             state, reward, done, _ = env.step(action)
+            cumulative_reward += reward
+        cumulative_reward_per_episode.append(cumulative_reward)
+        print(f"Episode Time Taken: {time() - start}")
+    
+    print(f"Cumulative reward: {cumulative_reward_per_episode}")
+    
+    plt.plot(cumulative_reward_per_episode)
+    plt.xlabel('Episode')
+    plt.ylabel('Cumulative Reward')
+    plt.title('HINN Pipeline Evaluation Cumulative Reward')
+
+    plt.savefig('HINN_EVAL_CR.png')
+    plt.show()
 
     print("END")
